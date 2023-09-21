61f5fc04224eab612441100e277faa7eb84e8570
sofiarobles <sofiarobles@localhost>
2023-09-21 14:35:14 -0500

    sieve

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,25 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("How many numbers do you want to sort through?");
+		int n = in.nextInt();
+		
+		int[] n1 = new int[n-2];
+		for (int i = 2; i<n; i++)
+		{
+				n1 [i-2] = i;
+			
+		}
+		for (int j = 0; j<n1.length; j++)
+		{
+			for (int k = )
+		}
+	}
+
+}
